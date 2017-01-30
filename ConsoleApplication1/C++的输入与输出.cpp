b6d08d32872eeb7a21f3480a35fc61929d96d0b5
Administrator <Administrator@WIN7U-20160224V>
2017-01-30 16:46:02 +0800

    添加项目文件。

---
+++ ConsoleApplication1/C++的输入与输出.cpp
@@ -0,0 +1,12 @@
+#include"stdafx.h"
+#include<iostream>
+#include<string>
+using namespace std;
+void main()
+{
+	cout<<"hello world\n";//输出函数，注意引号
+	int a;
+	cin >> a;//输出函数
+	cout <<a<<dec<<a<<oct<<a<<hex<<a;//十进制八进制十六进制
+	system("pause");
+}
